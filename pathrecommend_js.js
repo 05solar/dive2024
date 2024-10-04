fe1b09cec55f07a7eeef285708b90bb23f039cd2
05solar <lotus05f@gmail.com>
2024-10-05 01:54:18 +0900

    add help button in path reommend page

--- pathrecommend_js.js
+++ pathrecommend_js.js
@@ -11,5 +11,6 @@ document.getElementById('generate-route').addEventListener('click', function() {
     const transportation = document.getElementById('transportation').value;
     const kind = document.getElementById('kind').value;
 
-    alert(`Route generated with ${transportation} to ${kind}!`);
+    // 경로 선택 후 Map 페이지로 이동
+    window.location.href = 'map_html.html';  // Map 페이지의 실제 파일명 또는 URL로 변경
 });

e34226fb6e488a3a60dee2907f8a33e9312c9d40
05solar <lotus05f@gmail.com>
2024-10-05 15:50:57 +0900

    getpoint page create

---
+++ getpoint_js.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    // 불꽃 애니메이션을 초기화하여 페이지 로드 시 효과 실행
+    const firework = document.querySelector('.firework-image');
+    firework.style.animation = 'fireworkEffect 1.5s ease-out forwards';
+};
